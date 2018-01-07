588a997e1b0ca3bc3794755f0229863e5c0b848a
borrik <borrik@live.ru>
2018-01-07 16:26:30 +0200

    class phone gen added

--- functs.py
+++ functs.py
@@ -1,18 +1,26 @@
 import os
-prefs=['052','054','051','058']
-body=range(0, 9999999)
-otpname='phones.log'
-otpdir=os.path.abspath(os.curdir)
-fh=open(os.path.join(otpdir,otpname),'w')
-def phonegen(prefs,body):
-    for i in prefs:
-        for r in body:
-            res=(lambda x,y: ''.join((x,str(y).zfill(7))))
-            fh.writelines(res(i,r)+'\n')
-    fh.close()
-print("Building phonelist in: %s..." %fh.name)
 
-phonegen(prefs,body)
+class generate:
+    def __init__(self):
+        prefs = ['052', '054', '051', '058']
+        body = range(0, 9999999)
+        otpname = 'phones.log'
+        otpdir = os.path.abspath(os.curdir)
+        self.phonegen(prefs, body,otpdir,otpname)
 
-print('Done.')
+
+    def phonegen(self,prefs,body,otpdir,otpname):
+        fh = open(os.path.join(otpdir, otpname), 'w')
+        print("Building phonelist in: %s..." % fh.name)
+        for i in prefs:
+            for r in body:
+                res=(lambda x,y: ''.join((x,str(y).zfill(7))))
+                fh.writelines(res(i,r)+'\n')
+        fh.close()
+        print('Done.')
+
+
+rt=generate()
+
+
 
