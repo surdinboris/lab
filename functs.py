fecf6c613fb15ce0163d6c982b3dad2688c1d3bd
Boris <borrik@live.ru>
2018-01-10 20:55:59 +0200

    class vars appended

--- functs.py
+++ functs.py
@@ -1,23 +1,23 @@
 import os
 class generate:
+
     def __init__(self):
         self.prefs = ['052', '054', '051', '058']
         self.body = range(0, 9)
         self.otpname = 'phones.log'
         self.otpdir = os.path.abspath(os.curdir)
-        self.phonegen(self.prefs, self.body,self.otpdir,self.otpname)
 
-    def phonegen(self,prefs,body,otpdir,otpname):
-        fh = open(os.path.join(otpdir, otpname), 'w')
+    def phonegen(self):
+        fh = open(os.path.join(self.otpdir, self.otpname), 'w')
         print("Building phonelist in: %s..." % fh.name)
-        for i in prefs:
-            for r in body:
+        for i in self.prefs:
+            for r in self.body:
                 res=(lambda x,y: ''.join((x,str(y).zfill(7))))
                 fh.writelines(res(i,r)+'\n')
         fh.close()
+
         print('Done.')
 
-
-rt=generate()
-
-
+rt1=generate()
+rt1.prefs=['045','094']
+rt1.phonegen()
