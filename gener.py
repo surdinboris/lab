ed9f60f0a2b3335d880ede0fff6d8afa1842ce36
borrik <borrik@live.ru>
2018-01-11 16:24:26 +0200

    regex parser added

---
+++ gener.py
@@ -0,0 +1,16 @@
+from time import sleep
+t = True
+cou=0
+
+while t == True:
+    print('yyy')
+    cou+=1
+    if cou > 50:
+        print('co tresh')
+        t=False
+sleep(5)
+t=False
+
+
+
+
---
+++ replacer.py
@@ -0,0 +1,13 @@
+import re
+
+tmlt=open('C:/tmp/_template.txt','r', encoding="utf8")
+rows = tmlt.readlines()
+searchp=re.compile(r'<.*todo.*>')
+counter=0
+
+for row in rows:
+    match=searchp.match(row)
+    if match:
+        print(match)
+    #counter+=1
+#print(counter)
